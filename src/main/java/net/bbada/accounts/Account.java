26b470978b74e8b675e8bff51cda1f4b45a4b8ad
Jihoon Sung <sungjihoon@gmail.com>
2016-07-25 00:46:42 +0900

    Create Account

---
+++ src/main/java/net/bbada/accounts/Account.java
@@ -0,0 +1,34 @@
+package net.bbada.accounts;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by Jihoon Sung on 2016-06-29.
+ */
+@Entity
+@Getter
+@Setter
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String fullName;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date joined;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updated;
+}
